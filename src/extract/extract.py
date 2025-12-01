809aaf0155f5d10210511c0f8618dee4b0c993ee
Nasif Islam <178942734+Nasif-Islam@users.noreply.github.com>
2025-12-01 11:33:41 +0000

    Fixed docstring for extract function

--- src/extract/extract.py
+++ src/extract/extract.py
@@ -1,17 +1,19 @@
 import requests
 from src.utils.logger import get_logger
+from src.utils.timer import timer
 
 logger = get_logger(__name__)
 
 API_URL = "https://api.coingecko.com/api/v3/simple/price"
 
 
-def extract_crypto_prices(coins: list[str], currencies: list[str]) -> dict:
+@timer("Crypto Price Extraction")
+def extract_crypto_prices(coins: list[dict], currencies: list[str]) -> dict:
     """
     Extract crypto pries from CoinGecko.
 
     Args:
-        coins (list[str]): list of coin dicts from config
+        coins (list[dict]): list of coin dicts from config
         currencies (list[str]): list of fiat currencies e.g. ["gbp", "usd"]
 
     Returns:
@@ -29,24 +31,24 @@ def extract_crypto_prices(coins: list[str], currencies: list[str]) -> dict:
         "include_24hr_change": "true"
     }
 
-    logger.info("Starting extraction for coins: {coin_ids}")
+    logger.info(f"Starting extraction for coins: {coin_ids}")
 
     try:
         response = requests.get(API_URL, params=params, timeout=10)
         response.raise_for_status()
-    except requests.exceptions.RequestsException as e:
-        logger.error(f"APi request failed: {e}")
+    except requests.exceptions.RequestException as e:
+        logger.error(f"API request failed: {e}")
         raise
 
     try:
         data = response.json()
     except ValueError as e:
         logger.error(f"Failed to decode JSON: {e}")
-        return data
+        raise
 
     if not isinstance(data, dict):
         logger.error(f"Unexpected response type: {type(data)}")
-        raise ValueError
+        raise ValueError("API returned unexpected JSON format")
 
     logger.info(f"Extraction successful - received data for {len(data)} coins")
 
